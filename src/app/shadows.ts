4d12adce717b39d737870c43b3a49f1e868fa43b
Jeane Erwansyah <blcktmyie123@gmail.com>
2023-10-13 15:37:23 +0700

    chore(design system): add global css, shadow css, typography css

---
+++ src/app/shadows.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react'
+
+export const shadow2px = css({
+  background: 'var(--neutral-white, #FFF)',
+  boxShadow: '0px 2px 4px 0px rgba(171, 190, 209, 0.60)'
+})
+
+export const shadow4px = css({
+  background: 'var(--neutral-white, #FFF)',
+  boxShadow: '0px 4px 8px 0px rgba(171, 190, 209, 0.40)'
+})
+
+export const shadow8px = css({
+  background: 'var(--neutral-white, #FFF)',
+  boxShadow: '0px 8px 16px 0px rgba(171, 190, 209, 0.40); '
+})
